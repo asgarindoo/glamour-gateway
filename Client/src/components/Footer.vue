8118efa2b9548d6435de5a81914df4cd4dc9e425
Asgarindo Dwiki Ibrahim adji <asgarindo9@gmail.com>
2024-04-01 20:05:15 +0700

    modified some element

--- Client/src/components/Footer.vue
+++ Client/src/components/Footer.vue
@@ -5,36 +5,51 @@
         <div class="mb-8 md:mb-0">
           <h5 class="text-lg font-bold mb-4">About Us</h5>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            finibus felis ac leo consequat, eget aliquam tortor interdum.
+            We're dedicated to providing top-notch online shopping experiences.
+            Join us as we bring you quality products at great prices.
           </p>
         </div>
+
         <div class="mb-8 md:mb-0">
           <h5 class="text-lg font-bold mb-4">Quick Links</h5>
           <ul class="list-none">
-            <li><a href="#" class="text-gray-300 hover:text-white">Home</a></li>
             <li>
-              <a href="#" class="text-gray-300 hover:text-white">About</a>
+              <router-link to="/" class="text-gray-300 hover:text-white"
+                >Home</router-link
+              >
             </li>
             <li>
-              <a href="#" class="text-gray-300 hover:text-white">Services</a>
+              <router-link to="/about" class="text-gray-300 hover:text-white"
+                >About</router-link
+              >
             </li>
             <li>
-              <a href="#" class="text-gray-300 hover:text-white">Contact</a>
+              <router-link to="/products" class="text-gray-300 hover:text-white"
+                >Products</router-link
+              >
+            </li>
+            <li>
+              <router-link to="/about" class="text-gray-300 hover:text-white"
+                >Contact</router-link
+              >
             </li>
           </ul>
         </div>
+
         <div>
           <h5 class="text-lg font-bold mb-4">Contact Us</h5>
           <p class="text-gray-300">
-            Company Name<br />123 Street Name, City<br />Country<br />Phone:
+            info@glamourgateway.com<br />Jl. Ketintang, Surabaya<br />Indonesia<br />Phone:
             (123) 456-7890
           </p>
         </div>
       </div>
       <hr class="my-8 border-gray-700" />
       <div class="text-center">
-        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <p>
+          &copy; 2024 Made with <span style="color: #ffffff">&#10084;</span> in
+          Surabaya
+        </p>
       </div>
     </div>
   </footer>
--- Client/src/views/AboutView.vue
+++ Client/src/views/AboutView.vue
@@ -96,7 +96,7 @@
       <div
         class="w-full mb-10 md:w-1/2 pr-0 md:pr-8 animate__animated animate__fadeInLeft">
         <h2 class="text-3xl md:text-6xl font-semibold mb-8">Who we are</h2>
-        <p class="text-lg md:text-xl text-gray-700">
+        <p class="text-lg md:text-xl">
           Your premier destination for luxury beauty shopping from around the
           globe. We specialize in offering a personalized Jastip (Jasa Titip)
           service, ensuring that you have access to the most coveted beauty
@@ -126,34 +126,142 @@
 
     <!-- Our Mission section -->
     <div
-      class="mx-5 md:mx-20 mt-20 md:mt-36 text-center animate__animated animate__fadeInUp">
+      class="mx-5 md:mx-20 mt-20 md:mt-36 text-center animate__animated animate__fadeInDown">
       <h2 class="text-3xl md:text-6xl font-semibold mb-8">Our Mission</h2>
-      <p class="text-lg md:text-xl text-gray-700 mb-10">
-        At Glamour Gateway, we are committed to providing beauty enthusiasts
-        with unparalleled access to the world's finest beauty products. Our
-        mission is to make luxury beauty shopping a seamless and delightful
-        experience, allowing you to explore and indulge in the latest trends and
-        innovations from renowned brands worldwide.
-      </p>
+      <div class="bg-slate-800 py-10 px-6 rounded-lg shadow-lg">
+        <p class="text-lg md:text-xl text-white mb-10 px-14">
+          At Glamour Gateway, our mission is to
+          <span class="font-bold">ignite</span> your passion for beauty. We're
+          not just about products, we're about
+          <span class="font-bold">empowerment</span> and
+          <span class="font-bold">self-expression</span>. Our goal is to
+          <span class="font-bold">inspire</span> and
+          <span class="font-bold">empower</span> you to embrace your unique
+          beauty and style confidently. With us, luxury beauty isn't just a
+          purchase, it's a journey of self-discovery and celebration.
+        </p>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/7198/7198217.png"
+          alt="Mission Icon"
+          class="mx-auto w-24 h-24 rounded-full object-cover"
+          style="filter: brightness(0) invert(1)" />
+      </div>
     </div>
 
     <!-- How It Works section -->
     <div
-      class="mx-5 md:mx-20 mt-20 md:mt-36 text-center h-screen animate__animated animate__fadeInUp">
-      <h2 class="text-3xl md:text-6xl font-semibold mb-8">How It Works</h2>
-      <!-- Your existing code for "How It Works" section here -->
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio unde
-      quidem, fugiat necessitatibus reprehenderit autem eveniet minima iste
-      provident consequuntur voluptatum facere debitis quos aspernatur, nostrum
-      dicta, ipsam natus ipsum? Consequatur reiciendis deleniti labore inventore
-      nobis ab excepturi suscipit, eum nam consequuntur, quidem assumenda maxime
-      optio. Voluptate sequi pariatur quos quibusdam consequuntur adipisci
-      facere maxime libero hic sit autem, natus vel nobis exercitationem
-      similique dignissimos, distinctio cupiditate? Commodi libero quos
-      blanditiis nam quae nesciunt. Veniam quo, perspiciatis fugiat tempora
-      deleniti officiis, dicta, repellat voluptate esse saepe nihil quibusdam
-      adipisci? Eum voluptatem accusantium ab? Corporis itaque accusamus
-      blanditiis perferendis! Provident, nam.
+      class="mt-20 md:mt-36 text-center animate__animated animate__fadeInDown text-slate-900">
+      <h2 class="text-3xl md:text-5xl font-semibold mb-10 pt-10">
+        How It Works
+      </h2>
+      <div
+        class="grid grid-cols-1 md:grid-cols-5 gap-8 items-center justify-center mx-20">
+        <!-- Step 1 -->
+        <div class="max-w-xs mx-auto py-20">
+          <img
+            src="https://static.thenounproject.com/png/29506-200.png"
+            alt="Step 1"
+            class="w-[120px] h-[120px] border-8 border-slate-800" />
+          <p class="text-lg mt-4">Browse</p>
+        </div>
+        <!-- Step 2 -->
+        <div class="max-w-xs mx-auto">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWuIO5GJf42ApHPcw2EboYg9HRroItiC_CyicuslLnuM9zR1MJAK8fajIH7dFMfCIc6l8&usqp=CAU"
+            alt="Step 2"
+            class="w-[120px] h-[120px] border-8 border-slate-800" />
+          <p class="text-lg mt-4">Add to Cart</p>
+        </div>
+        <!-- Step 3 -->
+        <div class="max-w-xs mx-auto">
+          <img
+            src="https://www.svgrepo.com/show/374806/checkout.svg"
+            alt="Step 3"
+            class="w-[120px] h-[120px] border-8 border-slate-800" />
+          <p class="text-lg mt-4">Checkout</p>
+        </div>
+        <!-- Step 4 -->
+        <div class="max-w-xs mx-auto">
+          <img
+            src="https://media.istockphoto.com/id/1375716890/vector/same-day-delivery-icon-vector-for-graphic-design-logo-website-social-media-mobile-app-ui.jpg?s=612x612&w=0&k=20&c=f0tCmdUgj9Fq0pC1Ni9V6v6Jp0LV0UdrKXSUOUd-0gs="
+            alt="Step 4"
+            class="w-[120px] h-[120px] border-8 border-slate-800" />
+          <p class="text-lg mt-4">Delivery</p>
+        </div>
+        <!-- Step 5 -->
+        <div class="max-w-xs mx-auto">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/284/284128.png"
+            alt="Step 5"
+            class="w-[120px] h-[120px] border-8 border-slate-800" />
+          <p class="text-lg mt-4">Enjoy</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Contact Us section -->
+    <div
+      class="bg-slate-800 mt-20 md:mt-36 text-white py-20 px-8 md:px-20 flex flex-col justify-center items-center">
+      <div class="mx-auto md:flex justify-between items-center">
+        <!-- Contact Information -->
+        <div class="md:mr-16 mb-8 w-full md:w-1/2">
+          <h2 class="text-3xl md:text-5xl font-semibold mb-4">
+            Contact Information
+          </h2>
+          <p class="text-lg md:text-xl text-white my-6">
+            Have questions or need assistance? Feel free to reach out to us
+            using the contact information below.
+          </p>
+          <div class="mb-4 flex items-center">
+            <i class="fas fa-envelope mr-2"></i>
+            <div>
+              <p class="text-gray-400 mt-6">Email:</p>
+              <a
+                href="mailto:info@glamourgateway.com"
+                class="hover:text-gray-300 transition-colors duration-300"
+                >info@glamourgateway.com</a
+              >
+            </div>
+          </div>
+          <div class="flex items-center">
+            <i class="fas fa-phone mr-2"></i>
+            <div>
+              <p class="text-gray-400">Phone:</p>
+              <p>+1 (123) 456-7890</p>
+            </div>
+          </div>
+        </div>
+        <!-- Subscribe Newsletter Form -->
+        <div class="bg-white w-full lg:w-1/2 rounded-lg shadow-md p-8 md:p-12">
+          <h2 class="text-2xl md:text-3xl font-semibold mb-4 text-slate-900">
+            Subscribe Newsletter
+          </h2>
+          <form action="#" method="POST">
+            <div class="mb-6">
+              <label for="email" class="block text-gray-800 font-medium mb-2"
+                >Email:</label
+              >
+              <div class="relative">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  class="w-full border-gray-300 text-slate-900 rounded-md p-3 pl-10"
+                  placeholder="Your Email"
+                  required />
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <i class="fas fa-envelope text-slate-900"></i>
+                </div>
+              </div>
+            </div>
+            <button
+              type="submit"
+              class="bg-slate-800 text-white px-6 py-3 rounded-md hover:bg-slate-900 transition-colors duration-300">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
 
     <div>
